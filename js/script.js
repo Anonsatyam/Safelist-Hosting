3da310c5301528a3d74ebbc470f1f156ed6565b0
Amazeinternet <hello@amazeinternet.com>
2020-06-18 16:14:18 +0530

    Some Design Update were made to website

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+// window.onscroll = function () {
+//   myFunction()
+// };
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  var navbar = document.getElementById("nav-container");
+  var sticky = navbar.offsetTop;
+
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+});
+
+
+
+$(window).scroll(function () {
+  $('#nav-container').toggleClass('scrolled sticky', $(this).scrollTop() > 50);
+});
+
+// window.onscroll = function () {
+//   document.getElementById('logo-normal').style.display = 'none';
+// }
 
 let mainNav = document.getElementById("js-menu");
 let navBarToggle = document.getElementById("js-navbar-toggle");
@@ -7,23 +33,210 @@ navBarToggle.addEventListener("click", function () {
 });
 
 
-// document.getElementsByClassName("button-overlay").onclick = function () {
-
-//   // document.getElementById("button-overlay").style.display = "none";
-//   // document.getElementById("button-hide").style.display = "none";
-//   // document.getElementById("revealed").style.display = "initial";
-
-//   console.log("Hii");
+// document.getElementById("button-overlay-one").onclick = function () {
+//   document.getElementById("button-overlay-one").style.display = "none";
+//   document.getElementById("button-hide-one").style.display = "none";
+//   document.getElementById("revealed-one").style.display = "initial";
 // }
 
-document.querySelectorAll('.button-overlay').forEach(item => {
-  item.addEventListener('click', event => {
-    var allBtn = document.querySelectorAll(".button-overlay.button-hide.revealed");
 
-    for (var i = 0; i <= allBtn.length; i++) {
-      document.querySelectorAll(".button-overlay")[i].style.display = "none";
-      document.querySelectorAll(".button-hide")[i].style.display = "none";
-      document.querySelectorAll(".revealed")[i].style.display = "initial";
-    };
-  });
+// First Layout Code Starts From Here
+$("#button-overlay-one").on("click", function () {
+
+  $("#button-overlay-one").css("display", "none")
+  $("#button-hide-one").css("display", "none")
+  $("#revealed-one").css("display", "initial")
+
 });
+
+$("#button-overlay-two").on("click", function () {
+
+  $("#button-overlay-two").css("display", "none")
+  $("#button-hide-two").css("display", "none")
+  $("#revealed-two").css("display", "initial")
+
+});
+$("#button-overlay-three").on("click", function () {
+
+  $("#button-overlay-three").css("display", "none")
+  $("#button-hide-three").css("display", "none")
+  $("#revealed-three").css("display", "initial")
+
+});
+$("#button-overlay-four").on("click", function () {
+
+  $("#button-overlay-four").css("display", "none")
+  $("#button-hide-four").css("display", "none")
+  $("#revealed-four").css("display", "initial")
+
+});
+$("#button-overlay-five").on("click", function () {
+
+  $("#button-overlay-five").css("display", "none")
+  $("#button-hide-five").css("display", "none")
+  $("#revealed-five").css("display", "initial")
+
+});
+$("#button-overlay-six").on("click", function () {
+
+  $("#button-overlay-six").css("display", "none")
+  $("#button-hide-six").css("display", "none")
+  $("#revealed-six").css("display", "initial")
+
+});
+$("#button-overlay-seven").on("click", function () {
+
+  $("#button-overlay-seven").css("display", "none")
+  $("#button-hide-seven").css("display", "none")
+  $("#revealed-seven").css("display", "initial")
+
+});
+$("#button-overlay-eight").on("click", function () {
+
+  $("#button-overlay-eight").css("display", "none")
+  $("#button-hide-eight").css("display", "none")
+  $("#revealed-eight").css("display", "initial")
+
+});
+$("#button-overlay-nine").on("click", function () {
+
+  $("#button-overlay-nine").css("display", "none")
+  $("#button-hide-nine").css("display", "none")
+  $("#revealed-nine").css("display", "initial")
+
+});
+//First Layout Code Ends Here
+
+//LAYOUT TWO STARTS FROM HERE
+
+$("#button-overlay-ten").on("click", function () {
+
+  $("#button-overlay-ten").css("display", "none")
+  $("#button-hide-ten").css("display", "none")
+  $("#revealed-ten").css("display", "initial")
+
+});
+$("#button-overlay-11").on("click", function () {
+
+  $("#button-overlay-11").css("display", "none")
+  $("#button-hide-11").css("display", "none")
+  $("#revealed-11").css("display", "initial")
+
+});
+$("#button-overlay-12").on("click", function () {
+
+  $("#button-overlay-12").css("display", "none")
+  $("#button-hide-12").css("display", "none")
+  $("#revealed-12").css("display", "initial")
+
+});
+$("#button-overlay-13").on("click", function () {
+
+  $("#button-overlay-13").css("display", "none")
+  $("#button-hide-13").css("display", "none")
+  $("#revealed-13").css("display", "initial")
+
+});
+$("#button-overlay-14").on("click", function () {
+
+  $("#button-overlay-14").css("display", "none")
+  $("#button-hide-14").css("display", "none")
+  $("#revealed-14").css("display", "initial")
+
+});
+$("#button-overlay-15").on("click", function () {
+
+  $("#button-overlay-15").css("display", "none")
+  $("#button-hide-15").css("display", "none")
+  $("#revealed-15").css("display", "initial")
+
+});
+$("#button-overlay-16").on("click", function () {
+
+  $("#button-overlay-16").css("display", "none")
+  $("#button-hide-16").css("display", "none")
+  $("#revealed-16").css("display", "initial")
+
+});
+$("#button-overlay-17").on("click", function () {
+
+  $("#button-overlay-17").css("display", "none")
+  $("#button-hide-17").css("display", "none")
+  $("#revealed-17").css("display", "initial")
+
+});
+$("#button-overlay-18").on("click", function () {
+
+  $("#button-overlay-18").css("display", "none")
+  $("#button-hide-18").css("display", "none")
+  $("#revealed-18").css("display", "initial")
+
+});
+//LAYOUT TWO ENDS HERE
+$("#button-overlay-19").on("click", function () {
+
+  $("#button-overlay-19").css("display", "none")
+  $("#button-hide-19").css("display", "none")
+  $("#revealed-19").css("display", "initial")
+
+});
+$("#button-overlay-20").on("click", function () {
+
+  $("#button-overlay-20").css("display", "none")
+  $("#button-hide-20").css("display", "none")
+  $("#revealed-20").css("display", "initial")
+
+});
+$("#button-overlay-21").on("click", function () {
+
+  $("#button-overlay-21").css("display", "none")
+  $("#button-hide-21").css("display", "none")
+  $("#revealed-21").css("display", "initial")
+
+});
+$("#button-overlay-22").on("click", function () {
+
+  $("#button-overlay-22").css("display", "none")
+  $("#button-hide-22").css("display", "none")
+  $("#revealed-22").css("display", "initial")
+
+});
+$("#button-overlay-23").on("click", function () {
+
+  $("#button-overlay-23").css("display", "none")
+  $("#button-hide-23").css("display", "none")
+  $("#revealed-23").css("display", "initial")
+
+});
+$("#button-overlay-24").on("click", function () {
+
+  $("#button-overlay-24").css("display", "none")
+  $("#button-hide-24").css("display", "none")
+  $("#revealed-24").css("display", "initial")
+
+});
+$("#button-overlay-25").on("click", function () {
+
+  $("#button-overlay-25").css("display", "none")
+  $("#button-hide-25").css("display", "none")
+  $("#revealed-25").css("display", "initial")
+
+});
+$("#button-overlay-26").on("click", function () {
+
+  $("#button-overlay-26").css("display", "none")
+  $("#button-hide-26").css("display", "none")
+  $("#revealed-26").css("display", "initial")
+
+});
+$("#button-overlay-27").on("click", function () {
+
+  $("#button-overlay-27").css("display", "none")
+  $("#button-hide-27").css("display", "none")
+  $("#revealed-27").css("display", "initial")
+
+});
+
+//LAYOUT THREE STARTS FROM HERE
+
+
